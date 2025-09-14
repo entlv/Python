1a9c04f1e6b497b099e6523b6c83694f831af406
Mehdi <180872863+entlv@users.noreply.github.com>
2025-09-14 14:48:36 +0000

    Add files via upload

---
+++ Exercise/User input/rectangle area.py
@@ -0,0 +1,9 @@
+#==================================[Exercise 1( rectangle area calcater)]
+h=int(input("Enter the rerectangle's hight: "))
+w=int(input("Enter the rerectangle's wigth: "))
+area=h*w
+#print(area)
+#function
+def rectangle_area(h,l):
+    print(f"rectangle area is : {h*l}")
+rectangle_area(h,w)
---
+++ Exercise/User input/shoping cart.py
@@ -0,0 +1,5 @@
+item=input("what is your item :")
+price=float(input(" what the pirce of the item : "))
+quantity=int(input("how many did you get :"))
+total= quantity * price 
+print(f"you have bought : {item } : {total} $")
